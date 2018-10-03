7628641955a79ffe58c42b57ca4bd8b68d25c848
Angela Downs <angeladowns@me.com>
2018-10-03 17:02:24 -0400

    Exercise 9.

---
+++ ex9.py
@@ -0,0 +1,18 @@
+# Here's some new strange stuff, remember type it exactly.
+
+# defining variables
+# \n inserts a new line break in the string
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# prints the string and variable strings
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# prints a paragraph! Or, a multi-lined string.
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
