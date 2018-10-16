b196a22fcf07004e77d2192747cabfd04dccb594
Angela Downs <angeladowns@me.com>
2018-10-16 14:14:28 -0400

    Exercise 13.

---
+++ ex13.py
@@ -0,0 +1,12 @@
+from sys import argv
+# read the What You Should See section for how to run this
+# assigning variables to argv
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+print("Your second third variable is:", third)
+# using input() to prompt for data
+name = input("What's your name? ")
