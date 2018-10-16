1438359e028a7363cb89265f44eb50784b1ecb7a
Angela Downs <angeladowns@me.com>
2018-10-16 14:47:54 -0400

    Exercise 14.

---
+++ ex14.py
@@ -0,0 +1,27 @@
+from sys import argv
+
+# script is the file name, obvs and user_name is entered on the command line
+script, user_name, snack = argv
+# by creating a prompt variable, we can use it over and over without retyping it AND if we want to change it to something less kitty-like, we only have to change it once. Boom!
+prompt = '>.< meowwwkay? '
+
+print(f"Hi {user_name}, I'm the {script} script.")
+print("I'd like to ask you a few questions.")
+print(f"Do you like me, {user_name}?")
+likes = input(prompt)
+
+print(f"Where do you live, {user_name}?")
+lives = input(prompt)
+
+print("What kind of computer do you have?")
+computer = input(prompt)
+
+print("Are you hungry?")
+hungry = input(prompt)
+print(f"Looks like you're having {snack} for lunch.")
+
+print(f"""
+Alright, so you said {likes} about liking me.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice!
+""")
